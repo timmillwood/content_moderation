8f774c5ecc1d45718c255313cd1384c00093558a
Larry Garfield <garfield@palantir.net>
2015-12-03 18:04:40 -0600

    Use interfaces.

--- src/EntityTypeInfo.php
+++ src/EntityTypeInfo.php
@@ -6,9 +6,11 @@
 
 namespace Drupal\moderation_state;
 
-use Drupal\Core\Config\Entity\ConfigEntityType;
+use Drupal\Core\Config\Entity\ConfigEntityTypeInterface;
 use Drupal\Core\Entity\ContentEntityType;
-use Drupal\Core\Entity\EntityType;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\moderation_state\Form\EntityWorkflowForm;
+use Drupal\moderation_state\Routing\WorkflowRouteProvider;
 
 /**
  * Service class for manipulating entity type information.
@@ -20,28 +22,49 @@ class EntityTypeInfo {
    *
    * This is an alter hook bridge.
    *
-   * @param EntityType[] $entity_types
+   * @param EntityTypeInterface[] $entity_types
    *   The master entity type list to alter.
    */
   public function entityTypeAlter(array &$entity_types) {
     foreach ($this->revisionableEntityTypes($entity_types) as $type_name => $type) {
-      $entity_types[$type_name] = $this->addWorkflow($type, $type_name);
+      $entity_types[$type_name] = $this->addWorkflow($type);
     }
   }
 
-  protected function addWorkflow(ConfigEntityType $type, $type_name) {
-    drupal_set_message($type_name);
-    // @todo Add another form handler and link, and a route provider to generate routes to the new form.
+  /**
+   * Modifies an entity type to include workflow configuration support.
+   *
+   * That "configuration support" includes a configuration form, a hypermedia
+   * link, and a route provider to tie it all together.
+   *
+   * @param \Drupal\Core\Config\Entity\ConfigEntityTypeInterface $type
+   *   The config entity definition to modify.
+   * @return \Drupal\Core\Config\Entity\ConfigEntityTypeInterface
+   *   The modified config entity definition.
+   */
+  protected function addWorkflow(ConfigEntityTypeInterface $type) {
+    if ($type->hasLinkTemplate('edit-form')) {
+      $type->setLinkTemplate('workflow-form', $type->getLinkTemplate('edit-form') . '/workflow');
+    }
+
+    $type->setFormClass('workflow', EntityWorkflowForm::class);
+
+    // @todo Core forgot to add a direct way to manipulate route_provider, so
+    // we have to do it the sloppy way for now.
+    $providers = $type->getHandlerClass('route_provider') ?: [];
+    $providers['workflow'] = WorkflowRouteProvider::class;
+    $type->setHandlerClass('route_provider', $providers);
+
     return $type;
   }
 
   /**
    * Filters the provided entity types to just the ones we are interested in.
    *
-   * @param EntityType[] $entity_types
+   * @param EntityTypeInterface[] $entity_types
    *   The master entity type list to alter.
    * @return \CallbackFilterIterator
-   *   An iterator producing only the
+   *   An iterator producing only the config entities we want to modify.
    */
   protected function revisionableEntityTypes($entity_types) {
     $filtered_list = new \CallbackFilterIterator(
@@ -51,7 +74,7 @@ class EntityTypeInfo {
       [$this, 'bundleFilter']);
 
     // This one needs to be inlined since it calls back to the $entity_types variable.
-    $filtered_list = new \CallbackFilterIterator($filtered_list, function(EntityType $current, $key) use ($entity_types) {
+    $filtered_list = new \CallbackFilterIterator($filtered_list, function(EntityTypeInterface $current, $key) use ($entity_types) {
       /** @var ContentEntityType $content_entity */
       $content_entity = $entity_types[$current->get('bundle_of')];
       return $content_entity->isRevisionable();
@@ -70,8 +93,8 @@ class EntityTypeInfo {
    * @return bool
    *   True of the entity type is a Config entity, False otherwise.
    */
-  public function typeFilter(EntityType $type) {
-    return $type instanceof ConfigEntityType;
+  public function typeFilter(EntityTypeInterface $type) {
+    return $type instanceof ConfigEntityTypeInterface;
   }
 
   /**
@@ -83,7 +106,7 @@ class EntityTypeInfo {
    * @return bool
    *   True if the entity type is a Bundle definition for a Content entity.
    */
-  public function bundleFilter(ConfigEntityType $type) {
+  public function bundleFilter(ConfigEntityTypeInterface $type) {
     return (bool)$type->get('bundle_of');
   }
 }
