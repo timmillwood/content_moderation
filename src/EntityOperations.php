68c0f1b979782aa4a67baaa149113062838697fd
Larry Garfield <larry@garfieldtech.com>
2015-12-07 16:39:17 -0600

    Make EntityOperations entity type agnostic.

--- src/EntityOperations.php
+++ src/EntityOperations.php
@@ -9,9 +9,10 @@ namespace Drupal\moderation_state;
 
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
 
 /**
- * Defines a class for reacting to node events.
+ * Defines a class for reacting to entity events.
  */
 class EntityOperations {
 
@@ -33,30 +34,44 @@ class EntityOperations {
   /**
    * Acts on an entity and set the published status based on the moderation state.
    *
-   * @param \Drupal\Core\Entity\EntityInterface $node
+   * @param \Drupal\Core\Entity\EntityInterface $entity
    *   The entity being saved.
    */
-  public function entityPresave(EntityInterface $node) {
-    /* @var \Drupal\node\NodeTypeInterface $node_type */
-    $node_type = $this->entityTypeManager->getStorage('node_type')->load($node->bundle());
-    if (!$node_type->getThirdPartySetting('moderation_state', 'enabled', FALSE)) {
-      // @todo write a test for this.
+  public function entityPresave(EntityInterface $entity) {
+    if (!$this->isModeratableEntity($entity)) {
       return;
     }
+
     // @todo write a test for this.
-    if ($node->moderation_state->entity) {
-      $original = !empty($node->original) ? $node->original : NULL;
-      if ($original && $original->moderation_state->target_id !== $node->moderation_state->target_id) {
+    if ($entity->moderation_state->entity) {
+      $original = !empty($entity->original) ? $entity->original : NULL;
+      if ($original && $original->moderation_state->target_id !== $entity->moderation_state->target_id) {
         // We're moving to a new state, so force a new revision.
-        $node->setNewRevision(TRUE);
-        if ((!$original->moderation_state->entity && $original->isPublished()) || ($original->moderation_state->entity->isPublishedState() && !$node->moderation_state->entity->isPublishedState())) {
+        $entity->setNewRevision(TRUE);
+        if ((!$original->moderation_state->entity && $original->isPublished()) || ($original->moderation_state->entity->isPublishedState() && !$entity->moderation_state->entity->isPublishedState())) {
           // Mark this as a new forward revision.
-          $node->isDefaultRevision(FALSE);
+          $entity->isDefaultRevision(FALSE);
         }
       }
 
-      $node->setPublished($node->moderation_state->entity->isPublishedState());
+      $entity->setPublished($entity->moderation_state->entity->isPublishedState());
     }
   }
 
+  /**
+   * Determines if an entity is one we should be moderating.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity we may be moderating.
+   *
+   * @return bool
+   *   TRUE if this is an entity that we should act upon, FALSE otherwise.
+   */
+  protected function isModeratableEntity(EntityInterface $entity) {
+    $type_string = $entity->getEntityType()->getBundleEntityType();
+
+    /** @var EntityTypeInterface $entity_type */
+    $entity_type = $this->entityTypeManager->getStorage($type_string)->load($entity->bundle());
+    return $entity_type->getThirdPartySetting('moderation_state', 'enabled', FALSE);
+  }
 }
