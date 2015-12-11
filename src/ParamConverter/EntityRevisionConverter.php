18417a2ff4394c88445e530b7eac86468b5b16f7
Daniel Wehner <daniel@tag1consulting.com>
2015-12-11 18:03:20 +0100

    use the new trait

--- src/ParamConverter/EntityRevisionConverter.php
+++ src/ParamConverter/EntityRevisionConverter.php
@@ -9,12 +9,15 @@ namespace Drupal\moderation_state\ParamConverter;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\ParamConverter\EntityConverter;
 use Drupal\Core\TypedData\TranslatableInterface;
+use Drupal\moderation_state\LatestRevisionTrait;
 use Symfony\Component\Routing\Route;
 
 /**
  * Defines a class for making sure the edit-route loads the current draft.
  */
 class EntityRevisionConverter extends EntityConverter {
+
+  use LatestRevisionTrait;
 
   /**
    * {@inheritdoc}
@@ -27,33 +30,6 @@ class EntityRevisionConverter extends EntityConverter {
       }
       $entity_type = $this->entityManager->getDefinition($entity_type_id);
       return $operation == 'edit' && $entity_type && $entity_type->isRevisionable();
-    }
-  }
-
-  /**
-   * Loads the latest revision of a specific entity.
-   *
-   * @param string $entity_type_id
-   *   The entity type ID.
-   * @param int $entity_id
-   *   The entity ID.
-   *
-   * @return \Drupal\Core\Entity\EntityInterface|null
-   *   The latest entity revision or NULL, if the entity type / entity doesn't
-   *   exist.
-   */
-  protected function getLatestRevision($entity_type_id, $entity_id) {
-    if ($storage = $this->entityManager->getStorage($entity_type_id)) {
-      $revision_ids = $storage->getQuery()
-        ->allRevisions()
-        ->condition($this->entityManager->getDefinition($entity_type_id)->getKey('id'), $entity_id)
-        ->sort($this->entityManager->getDefinition($entity_type_id)->getKey('revision'), 'DESC')
-        ->pager(1)
-        ->execute();
-      if ($revision_ids) {
-        $revision_id = array_keys($revision_ids)[0];
-        return $storage->loadRevision($revision_id);
-      }
     }
   }
 
