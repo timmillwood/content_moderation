42a6d90b7ba0c300795fd5786f1e5060002095f7
Larry Garfield <garfield@palantir.net>
2015-12-23 12:03:32 -0600

    Fix forward-revision detection.

--- src/ModerationInformationInterface.php
+++ src/ModerationInformationInterface.php
@@ -112,6 +112,34 @@ interface ModerationInformationInterface {
   public function getLatestRevision($entity_type_id, $entity_id);
 
   /**
+   * Returns the revision ID of the latest revision of the given entity.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   * @param int $entity_id
+   *   The entity ID.
+   *
+   * @return int
+   *   The revision ID of the latest revision for the specified entity, or
+   *   NULL if there is no such entity.
+   */
+  public function getLatestRevisionId($entity_type_id, $entity_id);
+
+  /**
+   * Returns the revision ID of the default revision for the specified entity.
+   *
+   * @param string $entity_type_id
+   *   The entity type ID.
+   * @param int $entity_id
+   *   The entity ID.
+   *
+   * @return int
+   *   The revision ID of the default revision, or NULL if the entity was
+   *   not found.
+   */
+  public function getDefaultRevisionId($entity_type_id, $entity_id);
+
+  /**
    * Determines if an entity is a latest revision.
    *
    * @param \Drupal\Core\Entity\EntityInterface $entity
