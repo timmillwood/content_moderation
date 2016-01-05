7de89c7b32bd71672636e014874c710ae65c293c
Bec White <white@palantir.net>
2016-01-05 13:03:28 -0600

    refs #2637278: Additional moderation state published/live_revision test cases.

--- tests/src/Kernel/ModerationStateEntityTest.php
+++ tests/src/Kernel/ModerationStateEntityTest.php
@@ -32,9 +32,16 @@ class ModerationStateEntityTest extends KernelTestBase {
     $this->installEntitySchema('moderation_state');
   }
 
+  /**
+   * @covers ::isPublishedState()
+   * @covers ::isLiveRevisionState()
+   *
+   * @todo these might not quite be crud tests, since they're looking at the method logic
+   */
   public function testModerationStateCrud() {
+    $moderation_state_id = $this->randomMachineName();
     $moderation_state = ModerationState::create([
-      'id' => $this->randomMachineName(),
+      'id' => $moderation_state_id,
       'label' => $this->randomString(),
       'published' => false,
       'live_revision' => false,
@@ -43,15 +50,35 @@ class ModerationStateEntityTest extends KernelTestBase {
     $this->assertFalse($moderation_state->isPublishedState());
     $this->assertFalse($moderation_state->isLiveRevisionState());
 
-    // @todo update moderation state to have live_revision = 1, published = 0
-    // @todo verify isPublishedState == false, isLiveRevisionState == true
+    // For archived states, a moderation state may prompt the revision to
+    // become the default live revision, but not be published.
+    $moderation_state->set('published', false);
+    $moderation_state->set('live_revision', true);
+    $moderation_state->save();
 
-    // @todo update moderation state to have published = 1, live_revision = 1
-    // @todo verify isPublishedState == true, isLiveRevisionState == true
+    $this->assertFalse($moderation_state->isPublishedState());
+    $this->assertTrue($moderation_state->isLiveRevisionState());
 
-    // @todo update moderation state to have published = 1, live_revision = 0
-    // @todo verify isPublishedState == true, isLiveRevisionState == true
-    $this->assertTrue(FALSE, 'Pending');
+    // When a moderation state is a published state, it should also become the
+    // live revision.
+    $moderation_state->set('published', true);
+    $moderation_state->set('live_revision', true);
+    $moderation_state->save();
+    $this->assertTrue($moderation_state->isPublishedState());
+    $this->assertTrue($moderation_state->isLiveRevisionState());
+
+    $moderation_state->set('published', true);
+    $moderation_state->set('live_revision', false);
+    $moderation_state->save();
+    $this->assertTrue($moderation_state->isPublishedState());
+    $this->assertTrue($moderation_state->isLiveRevisionState());
+
+    // When we delete a moderation state, it should go away.
+    // @todo this probably isn't necessary -- this case should be covered by
+    //       regular entity tests, right? however, this test class name does
+    //       say "crud"...
+    $moderation_state->delete();
+    $this->assertNull(ModerationState::load($moderation_state_id));
   }
 
 }
