0fa3cca499e401a8df5101ec5c68c20b30dce7df
Larry Garfield <garfield@palantir.net>
2016-01-19 14:52:48 -0600

    Remove now-incorrect tests.

--- src/Tests/ModerationStateTransitionsTest.php
+++ src/Tests/ModerationStateTransitionsTest.php
@@ -42,10 +42,12 @@ class ModerationStateTransitionsTest extends ModerationStateTestBase {
    */
   public function testTransitionAdministration() {
     $this->drupalLogin($this->adminUser);
+
     $this->drupalGet('admin/structure/workbench-moderation');
     $this->clickLink('Moderation state transitions');
     $this->assertLink('Add Moderation state transition');
     $this->assertText('Draft » Needs review');
+
     // Edit the Draft » Needs review.
     $this->drupalGet('admin/structure/workbench-moderation/transitions/draft_needs_review');
     $this->assertFieldByName('label', 'Draft » Needs review');
@@ -61,6 +63,7 @@ class ModerationStateTransitionsTest extends ModerationStateTestBase {
       'label' => 'Draft » Needs review',
     ], t('Save'));
     $this->assertText('Saved the Draft » Needs review Moderation state transition.');
+
     // Add a new state.
     $this->drupalGet('admin/structure/workbench-moderation/states/add');
     $this->drupalPostForm(NULL, [
@@ -68,15 +71,10 @@ class ModerationStateTransitionsTest extends ModerationStateTestBase {
       'id' => 'expired',
     ], t('Save'));
     $this->assertText('Created the Expired Moderation state.');
+
+    // Add a new transition.
     $this->drupalGet('admin/structure/workbench-moderation/transitions');
     $this->clickLink(t('Add Moderation state transition'));
-    $this->drupalPostForm(NULL, [
-      'label' => 'Published » Expired',
-      'id' => 'published_expired',
-      'stateFrom' => 'published',
-      'stateTo' => 'published',
-    ], t('Save'));
-    $this->assertText('You cannot use the same state for both from and to');
     $this->drupalPostForm(NULL, [
       'label' => 'Published » Expired',
       'id' => 'published_expired',
@@ -84,6 +82,8 @@ class ModerationStateTransitionsTest extends ModerationStateTestBase {
       'stateTo' => 'expired',
     ], t('Save'));
     $this->assertText('Created the Published » Expired Moderation state transition.');
+
+    // Delete the new transition.
     $this->drupalGet('admin/structure/workbench-moderation/transitions/published_expired');
     $this->clickLink('Delete');
     $this->assertText('Are you sure you want to delete Published » Expired?');
