a21f6c5454f430dd96e9ec832c7613d84f71f3c8
Larry Garfield <garfield@palantir.net>
2016-04-04 11:12:16 -0500

    Lazy-ensure the tracking table.

--- src/RevisionTracker.php
+++ src/RevisionTracker.php
@@ -5,6 +5,7 @@ namespace Drupal\workbench_moderation;
 
 
 use Drupal\Core\Database\Connection;
+use Drupal\Core\Database\DatabaseExceptionWrapper;
 use Drupal\Core\Database\SchemaObjectExistsException;
 
 /**
@@ -43,10 +44,36 @@ class RevisionTracker implements RevisionTrackerInterface {
    * {@inheritdoc}
    */
   public function setLatestRevision($entity_type, $entity_id, $langcode, $revision_id) {
+    try {
+      $this->recordLatestRevision($entity_type, $entity_id, $langcode, $revision_id);
+    }
+    catch (DatabaseExceptionWrapper $e) {
+      $this->ensureTableExists();
+      $this->recordLatestRevision($entity_type, $entity_id, $langcode, $revision_id);
+    }
 
-    $this->ensureTableExists();
+    return $this;
+  }
 
-    $this->connection->merge($this->tableName)
+  /**
+   * Records the latest revision of a given entity.
+   *
+   * @param $entity_type
+   *   The machine name of the type of entity.
+   * @param $entity_id
+   *   The Entity ID in question.
+   * @param $langcode
+   *   The langcode of the revision we're saving. Each language has its own
+   *   effective tree of entity revisions, so in different languages
+   *   different revisions will be "latest".
+   * @param $revision_id
+   *   The revision ID that is now the latest revision.
+   *
+   * @return int
+   *   One of the valid returns from a merge query's execute method.
+   */
+  protected function recordLatestRevision($entity_type, $entity_id, $langcode, $revision_id) {
+    return $this->connection->merge($this->tableName)
       ->keys([
         'entity_type' => $entity_type,
         'entity_id' => $entity_id,
@@ -55,9 +82,7 @@ class RevisionTracker implements RevisionTrackerInterface {
       ->fields([
         'revision_id' => $revision_id,
       ])
-    ->execute();
-
-    return $this;
+      ->execute();
   }
 
   /**
