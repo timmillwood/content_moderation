581ed44f69fbd9ebb07cf854a9e9c5bfc728bc6b
Larry Garfield <larry@garfieldtech.com>
2016-04-08 17:09:53 -0500

    Default the default state to draft, if it exists, because it will 99% of the time.

--- src/Form/BundleModerationConfigurationForm.php
+++ src/Form/BundleModerationConfigurationForm.php
@@ -72,7 +72,7 @@ class BundleModerationConfigurationForm extends EntityForm {
       ];
     }
 
-    $states = \Drupal::entityTypeManager()->getStorage('moderation_state')->loadMultiple();
+    $states = $this->entityTypeManager->getStorage('moderation_state')->loadMultiple();
     $options = [];
     /** @var ModerationState $state */
     foreach ($states as $key => $state) {
@@ -96,7 +96,7 @@ class BundleModerationConfigurationForm extends EntityForm {
       '#empty_option' => t('-- Select --'),
       '#options' => $options,
       '#description' => t('Select the moderation state for new content'),
-      '#default_value' => $bundle->getThirdPartySetting('workbench_moderation', 'default_moderation_state', ''),
+      '#default_value' => $bundle->getThirdPartySetting('workbench_moderation', 'default_moderation_state', 'draft'),
       '#states' => [
         'visible' => [
           ':input[name=enable_moderation_state]' => ['checked' => TRUE],
