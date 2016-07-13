fdee0f3756033651d7e3ec696471ed4e163ada32
Daniel Wehner <dawehner@googlemail.com>
2016-07-13 12:24:25 +0200

    Expand the test coverage for the revision and data table joins

--- tests/src/Kernel/ViewsDataIntegrationTest.php
+++ tests/src/Kernel/ViewsDataIntegrationTest.php
@@ -35,6 +35,12 @@ class ViewsDataIntegrationTest extends ViewsKernelTestBase {
     $this->installSchema('node', 'node_access');
     $this->installConfig('content_moderation_test_views');
     $this->installConfig('content_moderation');
+
+    $node_type = NodeType::create([
+      'type' => 'page',
+    ]);
+    $node_type->setThirdPartySetting('content_moderation', 'enabled', TRUE);
+    $node_type->save();
   }
 
   /**
@@ -43,12 +49,6 @@ class ViewsDataIntegrationTest extends ViewsKernelTestBase {
    * @see content_moderation_views_data()
    */
   public function testViewsData() {
-    $node_type = NodeType::create([
-      'type' => 'page',
-    ]);
-    $node_type->setThirdPartySetting('content_moderation', 'enabled', TRUE);
-    $node_type->save();
-
     $node = Node::create([
       'type' => 'page',
       'title' => 'Test title first revision',
@@ -80,6 +80,77 @@ class ViewsDataIntegrationTest extends ViewsKernelTestBase {
       ],
     ];
     $this->assertIdenticalResultset($view, $expected_result, ['nid' => 'nid', 'content_revision_tracker_revision_id' => 'revision_id', 'moderation_state' => 'moderation_state', 'moderation_state_1' => 'moderation_state_1']);
+  }
+
+  /**
+   * Tests the join from the revision data table to the moderation state table.
+   */
+  public function testContentModerationStateRevisionJoin() {
+    $node = Node::create([
+      'type' => 'page',
+      'title' => 'Test title first revision',
+    ]);
+    $node->moderation_state->target_id = 'published';
+    $node->save();
+
+    $revision = clone $node;
+    $revision->setNewRevision(TRUE);
+    $revision->isDefaultRevision(FALSE);
+    $revision->title->value = 'Test title second revision';
+    $revision->moderation_state->target_id = 'draft';
+    $revision->save();
+
+    $view = Views::getView('test_content_moderation_revision_test');
+    $view->execute();
+
+    $expected_result = [
+      [
+        'revision_id' => $node->getRevisionId(),
+        'moderation_state' => 'published',
+      ],
+      [
+        'revision_id' => $revision->getRevisionId(),
+        'moderation_state' => 'draft',
+      ],
+    ];
+    $this->assertIdenticalResultset($view, $expected_result, ['revision_id' => 'revision_id', 'moderation_state' => 'moderation_state']);
+  }
+
+  /**
+   * Tests the join from the data table to the moderation state table.
+   */
+  public function testContentModerationStateBaseJoin() {
+    $node = Node::create([
+      'type' => 'page',
+      'title' => 'Test title first revision',
+    ]);
+    $node->moderation_state->target_id = 'published';
+    $node->save();
+
+    $revision = clone $node;
+    $revision->setNewRevision(TRUE);
+    $revision->isDefaultRevision(FALSE);
+    $revision->title->value = 'Test title second revision';
+    $revision->moderation_state->target_id = 'draft';
+    $revision->save();
+
+    $view = Views::getView('test_content_moderation_base_table_test');
+    $view->execute();
+
+    $expected_result = [
+      [
+        'nid' => $node->id(),
+        // @todo I would have expected that the content_moderation_state default
+        //   revision is the same one as in the node, but it isn't
+        // joins from the base table to the default revision of the content_moderation.
+        'moderation_state' => 'draft',
+        // joins from the revision table to the default revision of the content_moderation.
+        'moderation_state_1' => 'draft',
+        // joins from the revision table to the revision of the content_moderation.
+        'moderation_state_2' => 'published',
+      ],
+    ];
+    $this->assertIdenticalResultset($view, $expected_result, ['nid' => 'nid', 'moderation_state' => 'moderation_state', 'moderation_state_1' => 'moderation_state_1', 'moderation_state_2' => 'moderation_state_2']);
   }
 
 }
